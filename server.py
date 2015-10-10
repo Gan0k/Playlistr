c9b9d5568cd748480c0fa6eae3cc1e98c52dd056
Guido Arnau <guido.arnau@gmail.com>
2015-10-10 18:26:51 +0100

    Works somewhat

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, render_template 
+from flask import Flask, request, render_template, url_for 
 import playlistr
 
 app = Flask(__name__)
 
 @app.route('/')
 def form():
-	return render_template('form.html') 
+	css = url_for('static',filename='css/bootstrap.min.css')
+	return render_template('form.html',s=css) 
 
 @app.route('/', methods=['POST'])
 def form_post():
-	return playlistr.make_playlist(request.form['text'])  
+	return playlistr.make_playlist(request.form.get['text'])
 
 if __name__ == "__main__":
 	app.run()
