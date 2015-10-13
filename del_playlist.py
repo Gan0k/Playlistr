dd65063c98a4bc74e2a5253c5c02a6da0c980c2a
Guido Arnau <guido.arnau@gmail.com>
2015-10-13 19:41:12 +0100

    Added keys.py and results templat

--- del_playlist.py
+++ del_playlist.py
@@ -5,13 +5,14 @@ import httplib2
 import os
 import sys
 
+import keys
+
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run_flow
 
-DEVELOPER_KEY = "YOUR_API_KEY"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 CLIENT_SECRETS_FILE = "client_secrets.json"
