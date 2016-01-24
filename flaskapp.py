741ceb207b95b03027cbb3e59d3bb0db1ba7f932
Guido Arnau <guido.arnau@gmail.com>
2016-01-24 18:28:29 +0000

    Improve video retrival

--- flaskapp.py
+++ flaskapp.py
@@ -4,6 +4,8 @@ import playlistr
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
 
+NOT_FOUND_MSG = 'No videos were found :('
+
 @app.route('/')
 def form():
 	css = url_for('static',filename='css/bootstrap.min.css')
@@ -12,7 +14,7 @@ def form():
 @app.route('/', methods=['POST'])
 def form_post():
 	pl = playlistr.make_playlist(request.form.get('text'))
-	if pl == playlistr.NOT_FOUND_MSG: return notfound
+	if pl is None: return NOT_FOUND_MSG
 	else: return redirect(pl, code=302)
 
 if __name__ == "__main__":
--- playlistr.py
+++ playlistr.py
@@ -15,7 +15,6 @@ YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 YOUTUBE_PLAYLIST_LINK = 'http://www.youtube.com/watch_videos?video_ids='
 DEVELOPER_KEY = os.environ.get('YOUTUBE_API_KEY')
-NOT_FOUND_MSG = 'No videos were found :('
 
 def youtube_search(yt, query, max_results=1):
 
@@ -23,16 +22,14 @@ def youtube_search(yt, query, max_results=1):
 
 	# Call the search.list method to retrieve results matching the specified
 	# query term.
-	search_response = yt.search().list(
+	response = yt.search().list(
 		q=query,
-		part="id,snippet",
-		maxResults=max_results
+		part="id",
+		maxResults=max_results,
+		type="video",
 		).execute()
 
-	videos = []
-	for search_result in search_response.get("items", []):
-		if search_result["id"]["kind"] == "youtube#video":
-			videos.append(search_result["id"]["videoId"])
+	videos = [res["id"]["videoId"] for res in response.get("items", [])]
 
 	logger.debug('Video(s) found: %d', len(videos))
 
@@ -65,8 +62,5 @@ def make_playlist(tracklist):
 		except HttpError as e:
 			logger.error("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
 
-	if not videos:
-		return NOT_FOUND_MSG
-	else:
-		return YOUTUBE_PLAYLIST_LINK + ','.join(videos)
+	return YOUTUBE_PLAYLIST_LINK + ','.join(videos) if videos else None
 
