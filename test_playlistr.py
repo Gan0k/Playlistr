915d1d18d1bc90bc9d8f066c8a9ebf6cbf9580a4
Guido Arnau <guido.arnau@gmail.com>
2016-02-09 17:52:43 +0000

    Add tests for Flask

--- test_playlistr.py
+++ test_playlistr.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import os
 from apiclient.discovery import build
+from flaskapp import app
 
 import playlistr
 
@@ -16,7 +17,7 @@ class TestPlaylistr(unittest.TestCase):
 		self.assertEqual(videoId[0], '9bZkp7q19f0')
 
 	def test_make_playlist1(self):
-		tracklist = '    01. gangam style \n  \t never gonna give '
+		tracklist = '	 01. gangam style \n  \t never gonna give '
 		pl = playlistr.make_playlist(tracklist)
 		self.assertIn('9bZkp7q19f0', pl)
 
@@ -34,6 +35,23 @@ class TestPlaylistr(unittest.TestCase):
 		with open(os.path.join(script_dir,relative_dir),'r') as tracklist:
 			pl = playlistr.make_playlist(tracklist.read())
 			self.assertIn('mxvG-_KvWlw', pl)
+
+class TestFlask(unittest.TestCase):
+
+	def setUp(self):
+		self.app = app.test_client()
+		self.app.testing = True
+
+	def tearDown(self):
+		pass
+
+	def test_home_response(self):
+		result = self.app.get('/')
+		self.assertEqual(result.status_code, 200)
+
+	def test_home_data(self):
+		result = self.app.get('/')
+		self.assertIn('Playlistr', str(result.data))
 
 if __name__ == '__main__':
 	unittest.main()
